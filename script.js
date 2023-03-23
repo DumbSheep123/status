f2b08b9d55a3a7c49f3e053905ddbc98ed26254d
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-03-23 14:13:40 -0400

    ...

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-fetch('https://beta.wmata.info/status')
+// Get the status element
+const statusElement = document.getElementById('status');
+
+// Define a function to fetch the API status and update the UI
+window.onload = function updateStatus() {
+  fetch('https://beta.wmata.info/status')
     .then(response => response.json())
     .then(data => {
-        if (data.status === 'OK') {
-            statusElement.classList.remove('status-down');
-            statusElement.classList.add('status-up');
-            statusElement.querySelector('.status-text').textContent = 'All systems operational';
-        } else {
-            statusElement.classList.remove('status-up');
-            statusElement.classList.add('status-down');
-            statusElement.querySelector('.status-text').textContent = `Server is down (${data.status})`;
-        }
-
+      if (data.status === 'OK') {
+        statusElement.classList.remove('status-down');
+        statusElement.classList.add('status-up');
+        statusElement.querySelector('.status-text').textContent = 'All systems operational';
+      } else {
+        statusElement.classList.remove('status-up');
+        statusElement.classList.add('status-down');
+        statusElement.querySelector('.status-text').textContent = `Server is down (${data.status})`;
+      }
     })
     .catch(error => {
-        alert('There is an error in the way we check the status. Please try again.');
+      statusElement.classList.remove('status-up');
+      statusElement.classList.add('status-down');
+      statusElement.querySelector('.status-text').textContent = 'API access denied!';
     });
+}
+
+// Call the updateStatus function every 5 seconds
+setInterval(updateStatus, 5000);
