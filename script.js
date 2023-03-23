b65fd39f14df0f2e84a256bb6a6c64125fc7c10d
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-03-23 14:08:52 -0400

    ...

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 fetch('https://beta.wmata.info/status')
-  .then(response => response.json())
-  .then(data => {
-    if (data.status === 'OK') {
-      alert('API is up and running!');
-    } else {
-      alert('API is currently down.');
-    }
-  })
-  .catch(error => {
-    alert('API access denied!');
-  });
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'OK') {
+            statusElement.classList.remove('status-down');
+            statusElement.classList.add('status-up');
+            statusElement.querySelector('.status-text').textContent = 'All systems operational';
+        } else {
+            statusElement.classList.remove('status-up');
+            statusElement.classList.add('status-down');
+            statusElement.querySelector('.status-text').textContent = `Server is down (${data.status})`;
+        }
+
+    })
+    .catch(error => {
+        alert('There is an error in the way we check the status. Please try again.');
+    });
